b1bfba3a8f5c84203cb8867d66714c5c99fcb401
Dierk Koenig <dierk.koenig@canoo.com>
2018-03-27 15:39:58 +0200

    adapt tests to not be bitten by browser's alert handling

--- src/integration-test/groovy/mvc/InPlaceCalculatorSpec.groovy
+++ src/integration-test/groovy/mvc/InPlaceCalculatorSpec.groovy
@@ -33,34 +33,31 @@ class InPlaceCalculatorSpec extends GebSpec {
         	title == "In-Place Calculator"
 
         when: "set invalid input"
-            $("form").en   = 0.9
-            $("form").exam = 3.0
+            $("form").en   = 0
+            $("form").exam = 3
             $("input", type: "submit").click()
 
         then: "Result contains error message"
             $("output").text() == "Cannot calculate. Input data was invalid."
         then: "invalid en field has error class while valid exam input has no class"
             $("#en",   class:'error')
-//            $("#exam").attr('class') == ""
+            $("#exam").attr('class') == ""
     }
 
 
     void "Invalid input is handled in-place by JS without submission"() {
         given: "a valid state"
             go '/InPlaceCalculator.html'
-            $("form").en   = 3.0
-            $("form").exam = 3.0
+            $("form").en   = 3
+            $("form").exam = 3
         when:
             $("input", type: "submit").click()
         then: "we should have a clean, valid state to start from"
-//            $("#en").attr('class') == ""
+            $("#en").attr('class') == ""
         when: "we enter some invalid value _without_ submitting"
-//            def message = withAlert {
-                $("form").en = 0.9
-//            }
+            $("form").en = 0
         then: "the in-place JS logic should kick in"
 //            $("#en").attr('class') == "error"
-//            message == "en value needs to be at least 1.0"
 //            $("#en").focused
     }
 
